a6f6cdfc65fe29d0c423ac15028faa638f5914e8
kumar <147685522+Lankalapallikumar@users.noreply.github.com>
2026-07-23 16:03:59 +0530

    Reformat train_model.py

--- train_model.py
+++ train_model.py
@@ -3,7 +3,6 @@ from sklearn.model_selection import train_test_split
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.linear_model import LogisticRegression
 import pickle
-
 fake_df = pd.read_csv("Fake.csv")
 real_df = pd.read_csv("True.csv")
 
